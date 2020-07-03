e71e8283f6aa36787bf293c498259a0c6e3853f9
kevin <tenngawang6080@gmail.com>
2020-07-03 15:16:03 -0400

    API basic setup done, association and database created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+    def index
+        users = User.all
+        render json: User
+    end
+
+    def show
+        user = User.find(params[:id])
+
+        render user
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create( user_name: "John", password: "123", role: "Software Engineer")
+user2 = User.create( user_name: "Wich", password: "1111", role: "Frontend Engineer")
+user3 = User.create( user_name: "William", password: "121212", role: "Backend Engineer")
+
+job1 = Job.create(user_id: user1.id, title: "Jr Software Engineer", company: "Forbes", date: "1st july 2020", link: "https://first.com", status: "rejected")
+job1 = Job.create(user_id: user1.id, title: "Software Engineer", company: "NY Times", date: "21st May 2020", link: "https://first.com", status: "active")
+job1 = Job.create(user_id: user2.id, title: "Web Engineer", company: "Facebook", date: "9th May 2020", link: "https://first.com", status: "active")
+job1 = Job.create(user_id: user3.id, title: "Skilled Backend Engineer", company: "Google", date: "30th June 2020", link: "https://first.com", status: "pause")
