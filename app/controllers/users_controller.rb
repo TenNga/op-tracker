2e4a316253ebbaea7a66ee687cefdf3ab806438c
kevin <tenngawang6080@gmail.com>
2020-07-04 20:03:50 -0400

    create action added to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,18 @@ class UsersController < ApplicationController
     def show
         user = User.find(params[:id])
 
-        render user
+        render json: user
+    end
+
+    def create
+        user = User.create(user_params)
+
+        render json: user
+    end
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:user_name, :password, :role)
     end
 end
